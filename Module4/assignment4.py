48ae3fdf3f04991aefa5b472b1a8e9c1eee80779
Fernando O. Gallego <fogallego@us.es>
2017-07-25 20:12:53 +0200

    Finished assignment 4 of Transforming data/Isomap

--- Module4/assignment4.py
+++ Module4/assignment4.py
@@ -25,8 +25,28 @@ def Plot2D(T, title, x, y, num_to_plot=40):
 
   # It also plots the full scatter:
   ax.scatter(T[:,x],T[:,y], marker='.',alpha=0.7)
+  
+def Plot3D(T, title, x, y, z, num_to_plot=40):
+  # This method picks a bunch of random samples (images in your case)
+  # to plot onto the chart:
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection = '3d')
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  ax.set_zlabel('Component: {0}'.format(z))
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2.
+    x1, y1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2.
+    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
-
+  # It also plots the full scatter:
+  ax.scatter(T[:,x],T[:,y],T[:,z], c='r', marker='.',alpha=0.7)
+  
 
 # A .MAT file is a .MATLAB file. The faces dataset could have came
 # in through .png images, but we'll show you how to do that in
@@ -40,7 +60,6 @@ num_pixels = int(math.sqrt(num_pixels))
 for i in range(num_images):
   df.loc[i,:] = df.loc[i,:].reshape(num_pixels, num_pixels).T.reshape(-1)
 
-
 #
 # TODO: Implement PCA here. Reduce the dataframe df down
 # to THREE components. Once you've done that, call Plot2D.
@@ -51,22 +70,38 @@ for i in range(num_images):
 # x is the principal component you want displayed on the x-axis, Can be 0 or 1
 # y is the principal component you want displayed on the y-axis, Can be 1 or 2
 #
-# .. your code here ..
+from sklearn.decomposition import PCA
 
+pca = PCA(n_components = 3, svd_solver='full')
+  
+T = pca.fit_transform(df)
+
+Plot2D(T, 'PCA', 1, 2)
 
 #
 # TODO: Implement Isomap here. Reduce the dataframe df down
 # to THREE components. Once you've done that, call Plot2D using
 # the first two components.
 #
-# .. your code here ..
+
+from sklearn import manifold
+
+iso = manifold.Isomap(n_neighbors=3, n_components=3)
+
+iso.fit(df)
+
+manifold = iso.transform(df)
+
+Plot2D(manifold, 'Isomap', 1, 2)
 
 
 #
 # TODO: If you're up for a challenge, draw your dataframes in 3D
 # Even if you're not, just do it anyway.
 #
-# .. your code here ..
+
+#Plot3D(T, 'PCA', 0, 1, 2)
+#Plot3D(manifold, 'Isomap', 0, 1, 2)
 
 
 plt.show()
