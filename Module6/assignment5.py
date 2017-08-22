67a01aa35c9cfed124f973919e5cd240e1152047
Fernando O. Gallego <fogallego@us.es>
2017-08-22 18:15:33 +0200

    Finished assignment 6 of Modelling2/DTree

--- Module6/assignment5.py
+++ Module6/assignment5.py
@@ -11,11 +11,15 @@ import pandas as pd
 # Header information is on the dataset's website at the UCI ML Repo
 # Check NA Encoding
 #
-# .. your code here ..
+X = pd.read_csv('Datasets/agaricus-lepiota.data', names=['class','cap-shape',
+'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing', 
+'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-aring',
+'stalk-surface-bring', 'stalk-color-aring', 'stalk-color-bring', 'veil-type',
+'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 
+'habitat'])
 
 # INFO: An easy way to show which rows have nans in them
-#print X[pd.isnull(X).any(axis=1)]
-
+print X[pd.isnull(X).any(axis=1)]
 
 # 
 # TODO: Go ahead and drop any row with a nan
@@ -29,13 +33,14 @@ print X.shape
 # them from X. Encode the labels, using the .map() trick we showed
 # you in Module 5 -- canadian:0, kama:1, and rosa:2
 #
-# .. your code here ..
+y = X['class']
+X = X.drop('class', axis=1)
 
 
 #
 # TODO: Encode the entire dataset using dummies
 #
-# .. your code here ..
+X = pd.get_dummies(X)
 
 
 # 
@@ -43,21 +48,27 @@ print X.shape
 # Your test size can be 30% with random_state 7
 # Use variable names: X_train, X_test, y_train, y_test
 #
-# .. your code here ..
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=7) 
 
 
 
 #
 # TODO: Create an DT classifier. No need to set any parameters
 #
-# .. your code here ..
+from sklearn import tree
+dtc = tree.DecisionTreeClassifier()
 
  
 #
 # TODO: train the classifier on the training data / labels:
 # TODO: score the classifier on the testing data / labels:
 #
-# .. your code here ..
+
+dtc.fit(X_train, y_train)
+
+score = dtc.score(X_test, y_test)
+
 print "High-Dimensionality Score: ", round((score*100), 3)
 
 
@@ -69,6 +80,9 @@ print "High-Dimensionality Score: ", round((score*100), 3)
 # the graphviz website. Also, a graph editor, gvedit.exe can be used to view the
 # tree directly from the exported tree.dot file without having to issue a call.
 #
-# .. your code here ..
+tree.export_graphviz(dtc.tree_, out_file='tree.dot', feature_names=X.columns)
+
+from subprocess import call
+call(['dot', '-T', 'png', 'tree.dot', '-o', 'tree.png'])
 
 
